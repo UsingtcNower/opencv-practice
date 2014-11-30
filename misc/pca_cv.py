8e72894997fb78068c4c66021d3dff080551d6f1
tao zhang <nowerzt@gmail.com>
2014-11-30 23:57:00 +0800

    Update pca_cv.py

--- misc/pca_cv.py
+++ misc/pca_cv.py
@@ -7,5 +7,14 @@ def pca(X, numOfPca=0):
     print Pusai
     X = X - Pusai
     print X
-    Cov = cv.mulTransposed(X, True)
+    Cov = cv.mulTransposed(X, False)
+    print Cov
+    [retval, eigenvalues, eigenvectors] = cv.eigen(Cov, True)
+    indexes = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[indexes]
+    eigenvectors = eigenvectors[:,indexes]
+    eigenvalues = eigenvalues[0:numOfPca].copy()
+    eigenvectors = eigenvectors[:,0:numOfPca].copy()
+    return [eigenvalues, eigenvectors, Pusai]
+
     
