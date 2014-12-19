ee842b3f101f52dcc11afc233222052a2fc2d7d1
UsingtcNower <nowerzt@gmail.com>
2014-12-19 23:13:43 +0800

    revise

--- src/ImageConvert.h
+++
@@ -1,9 +0,0 @@
-#ifndef CVUTIL_H
-#define CVUTIL_H
-#include "opencv/cxcore.h"
-#include <QImage>
-
-QImage *IplImage2QImage_s(IplImage *src);
-IplImage *QImage2IplImage_s(QImage *src);
-
-#endif // CVUTIL_H
