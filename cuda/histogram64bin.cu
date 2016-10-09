20ec9b85dce8efe5bb2851ac30335edaa1cfe300
tao zhang <nowerzt@gmail.com>
2016-10-09 09:07:26 +0800

    update

--- cuda/histogram64bin.cu
+++ cuda/histogram64bin.cu
@@ -1,14 +1,35 @@
 #include <helper_cuda.h>
+#include <helper_functions.h>
+#include <helper_math.h>
 #include <opencv2/opencv.hpp>
 
 #define SHARED_MEMORY_BANKS 16
+#define THREADBLOCK_SIZE SHARED_MEMORY_BANKS*4
 
 cv::Mat histImg;
 
-__global__ void hist64binKernel()
-{}
+__global__ void hist64binKernel(uchar* drData, int* partialHist)
+{
+    __shared__ uchar s_hist[64*THREADBLOCK_SIZE];
+    for (int i = 0; i < 64; ++i) {
+        s_hist[i*THREADBLOCK_SIZE + threadIdx.x] = 0;
+    }
+    __syncthreads();
+    int threadPos = blockDim.x*blockIdx.x + threadIdx.x;
+    s_hist[drData[threadPos] * THREADBLOCK_SIZE + threadIdx.x] ++;
+    __syncthreads();
+    if (threadIdx.x == 0) {
+        for (int i = 0; i < 64; ++i) {
+            int sum = 0;
+            for (int j = 0; j < blockDim.x; j++) {
+                sum += s_hist[i*THREADBLOCK_SIZE+threadIdx.x];
+            }
+            partialHist[i*gridDim.x + blockIdx.x] = sum;
+        }
+    }
+}
 
-__global__ void hist64binMerge()
+__global__ void hist64binMerge(int* partialHist, int* hist)
 {}
 
 int main(int argc, char** argv)
@@ -25,12 +46,19 @@ int main(int argc, char** argv)
 
     // init
     int devId = findCudaDevice(argc, (const char**)argv);
-    int* drData = NULL;
-    checkCudaErrors(cudaMalloc((void**)&drData, img.cols*img.rows*sizeof(int)));
+    uchar* drData = NULL;
+    checkCudaErrors(cudaMalloc((void**)&drData, img.cols*img.rows*sizeof(uchar)));
+    checkCudaErrors(cudaMemcpy(drData, img.data, img.cols*img.rows*sizeof(uchar), cudaMemcpyHostToDevice));
     int* hist = NULL;
     checkCudaErrors(cudaMalloc((void**)&hist, histSize*sizeof(int)));
-
+    dim3 dimBlock(THREADBLOCK_SIZE, 1);
+    dim3 dimGrid((img.cols*img.rows+dimBlock.x-1)/dimBlock.x, 1);
+    int* partialHist = NULL;
+    checkCudaErrors(cudaMalloc((void**)&partialHist, dimGrid.x*histSize*sizeof(int)));
+    
+    
     // calculate
+    hist64binKernel << <dimGrid, dimBlock >> >(drData, partialHist);
 
 
     // draw
