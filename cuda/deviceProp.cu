fb9ffe7897746b19aa4452be6873adcef0879250
tao zhang <nowerzt@gmail.com>
2016-10-12 15:48:43 +0800

    update

--- cuda/deviceProp.cu
+++ cuda/deviceProp.cu
@@ -6,5 +6,6 @@ int main(int argv, char** argc)
     int devId;
     cudaDeviceProp devProp;
     checkCudaErrors(cudaGetDevice(&devId));
-
+    checkCudaErrors(cudaGetDeviceProperties(&devProp, devId));
+    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devId, devProp.name, devProp.major, devProp.minor);
 }
